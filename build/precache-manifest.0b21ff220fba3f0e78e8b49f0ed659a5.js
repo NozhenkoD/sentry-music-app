b45716f1bcc465c5657f4c2732394801622ca304
Dmitry Nozhenko <nozhenkod@gmail.com>
2019-05-07 08:59:40 +0300

    readme

---
+++ build/precache-manifest.0b21ff220fba3f0e78e8b49f0ed659a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "249c224d13f34d2b26a5",
+    "url": "/sentry-music-app/static/js/runtime~main.249c224d.js"
+  },
+  {
+    "revision": "6943ac0a91b25c6442d7",
+    "url": "/sentry-music-app/static/js/main.6943ac0a.chunk.js"
+  },
+  {
+    "revision": "aa0f2a0b2c6f692a19d4",
+    "url": "/sentry-music-app/static/js/1.aa0f2a0b.chunk.js"
+  },
+  {
+    "revision": "6943ac0a91b25c6442d7",
+    "url": "/sentry-music-app/static/css/main.3a258d7c.chunk.css"
+  },
+  {
+    "revision": "aa0f2a0b2c6f692a19d4",
+    "url": "/sentry-music-app/static/css/1.bced2095.chunk.css"
+  },
+  {
+    "revision": "9fc6b407e164cc0ecad8d0476f539583",
+    "url": "/sentry-music-app/index.html"
+  }
+];
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 #### Music App
+
+Example repo for article [url](https://medium.com/p/56eb9af6e3bf)
+
+[Demo](https://nozhenkod.github.io/sentry-music-app/)
